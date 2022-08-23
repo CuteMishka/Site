ebb1c66aaf7f2097f1b807997a43431523a9c2d6
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-08-23 16:42:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-
+function quest1() {
+	var answer = document.getElementById('answ1')
+	if (answ1.style.display === "none") {
+		answ1.style.display = "block"
+	} else {
+		answ1.style.display = "none"
+	}
+}
+function quest2() {
+	var answer = document.getElementById('answ2')
+	if (answ2.style.display === "none") {
+		answ2.style.display = "block"
+	} else {
+		answ2.style.display = "none"
+	}
+}
+function quest3() {
+	var answer = document.getElementById('answ3')
+	if (answ3.style.display === "none") {
+		answ3.style.display = "block"
+	} else {
+		answ3.style.display = "none"
+	}
+}
