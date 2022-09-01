1904dc4266cfbeacd3968613ce132871a4533df1
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-09-01 16:12:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function quest(poryadkovyiNomer) {
+	var answers = document.querySelectorAll("div.answer");
+	for (answer of answers){
+		answer.style.display = "none"
+		var answer = document.getElementById('answ' + poryadkovyiNomer)
+	}
+	if (answer.style.display === "none") {
+		answer.style.display = "block"
+	} else {
+		answer.style.display = "none"
+	}
+}
